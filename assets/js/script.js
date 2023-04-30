fbc7a48de21c728840768deaca03c6191d525091
dev-timm <timm.kuemmeth@gmail.com>
2023-04-30 21:36:40 +0200

    change images, add favicon and foods array to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,112 @@
+const foods = [
+    {
+        id: 0,
+        name: 'apple',
+        img: 'assets/images/card-apple.png',
+        translation : {
+            en: 'apple',
+            de: 'Apfel'
+        }
+    },
+    {
+        id: 1,
+        name: 'avocado',
+        img: 'assets/images/card-avocado.png',
+        translation : {
+            en: 'avocado',
+            de: 'Avocado'
+        }
+    },
+    {
+        id: 2,
+        name: 'banana',
+        img: 'assets/images/card-banana.png',
+        translation : {
+            en: 'banana',
+            de: 'Banane'
+        }
+    },
+    {
+        id: 3,
+        name: 'broccoli',
+        img: 'assets/images/card-brokoli.png',
+        translation : {
+            en: 'broccoli',
+            de: 'Brokkoli'
+        }
+    },
+    {
+        id: 4,
+        name: 'cauliflower',
+        img: 'assets/images/card-cauliflower.png',
+        translation : {
+            en: 'cauliflower',
+            de: 'Blumenkohl'
+        }
+    },
+    {
+        id: 5,
+        name: 'cherry',
+        img: 'assets/images/card-cherry.png',
+        translation : {
+            en: 'cherry',
+            de: 'Kirsche'
+        }
+    },
+    {
+        id: 6,
+        name: 'chilli',
+        img: 'assets/images/card-chilli.png',
+        translation : {
+            en: 'chilli',
+            de: 'Chilli'
+        }
+    },
+    {
+        id: 7,
+        name: 'coconut',
+        img: 'assets/images/card-coconut.png',
+        translation : {
+            en: 'coconut',
+            de: 'Kokosnuss'
+        }
+    },
+    {
+        id: 8,
+        name: 'pepper',
+        img: 'assets/images/card-pepper.png',
+        translation : {
+            en: 'pepper',
+            de: 'Paprika'
+        }
+    },
+    {
+        id: 9,
+        name: 'strawberry',
+        img: 'assets/images/card-strawberry.png',
+        translation : {
+            en: 'strawberry',
+            de: 'Erdbeere'
+        }
+    },
+    {
+        id: 10,
+        name: 'tangerine',
+        img: 'assets/images/card-tangerine.png',
+        translation : {
+            en: 'tangerine',
+            de: 'Orange'
+        }
+    },
+    {
+        id: 11,
+        name: 'tomato',
+        img: 'assets/images/card-tomato.png',
+        translation : {
+            en: 'tomato',
+            de: 'Tomate'
+        }
+    }
+];
+
+
